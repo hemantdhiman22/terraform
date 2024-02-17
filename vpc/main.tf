f9e087baadd5e502a9ff2cebf7491d52e5034077
Hemant Dhiman <106331731+hemantdhiman22@users.noreply.github.com>
2024-02-17 22:44:57 +0530

    Add files via upload

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,108 +1,22 @@
-resource "aws_vpc" "prod" {
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>5.36.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "ap-south-1"
+}
+
+
+module "vpc" {
+  source = "./vpc"
+  project_name = var.project_name
   cidr_block = var.cidr_block
-  instance_tenancy = "default"
-  
-  tags = {
-    Name = "${var.project_name}-VPC"
-  }
-}
-
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-//Private subnets
-
-resource "aws_subnet" "private" {
-  vpc_id = aws_vpc.prod.id
-  count = length(var.private_subnet_cidr)
-  availability_zone = element(data.aws_availability_zones.available.names, count.index)
-  cidr_block = element(var.private_subnet_cidr, count.index)
-  map_public_ip_on_launch = "false"
-  tags = {
-    Name = "${var.project_name}-private_subnet-${count.index}"
-  }
-}
-
-//Public subnets
-
-resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.prod.id
-  count = length(var.public_subnet_cidr)
-  availability_zone = element(data.aws_availability_zones.available.names, count.index)
-  cidr_block = element(var.public_subnet_cidr, count.index)
-  map_public_ip_on_launch = "true"
-  tags = {
-    Name = "${var.project_name}-public_subnet-${count.index}"
-  }
-}
-
-
-//Internet Gateway
-
-resource "aws_internet_gateway" "IGW-1" {
-  vpc_id = aws_vpc.prod.id
-  tags = {
-    Name = "${var.project_name}-IGW"
-  }
-}
-
-//public route-table
-
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.prod.id
-  
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.IGW-1.id
-  }
-  tags = {
-    Name = "${var.project_name}-pulic-route-table"
-  }
-}
-
-//public route table association
-
-resource "aws_route_table_association" "public" {
-  count = length(var.public_subnet_cidr)
-  subnet_id = element(aws_subnet.public.*.id, count.index)
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-//Private route table
-
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.prod.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat.id
-}
-  tags = {
-    Name = "${var.project_name}-private-route-table"
-  }
-}
-
-//Private route table association
-
-resource "aws_route_table_association" "private" {
-  count = length(var.private_subnet_cidr)
-  subnet_id = element(aws_subnet.private.*.id, count.index)
-  route_table_id = aws_route_table.private_route_table.id
-}
-
-
-//Elastic IP for NAT
-
-resource "aws_eip" "eip" {
-  tags = {
-    Name = "${var.project_name}-eip"
-  }
-}
-
-//NAT Gateway
-
-resource "aws_nat_gateway" "nat" {
-  //count = length(aws_subnet.private)
-  allocation_id = aws_eip.eip.id
-  subnet_id = aws_subnet.public[0].id
+  private_subnet_cidr = var.private_subnet_cidr
+  public_subnet_cidr = var.public_subnet_cidr
 }
---
+++ vpc/terraform.tfvars
@@ -0,0 +1,5 @@
+region="ap-south-1"
+project_name = "UAT"
+cidr_block="192.168.0.0/16"
+private_subnet_cidr = ["192.168.0.0/24","192.168.1.0/24","192.168.2.0/24"]
+public_subnet_cidr = ["192.168.3.0/24","192.168.4.0/24","192.168.5.0/24"]
